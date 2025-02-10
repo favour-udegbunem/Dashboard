57110f78d7b1ef0d5a948cdc410fcfb09f72712b
Favour <favourudegbunem19@gmail.com>
2025-02-10 08:58:13 +0100

    Updated BaseLayout and dependencies

--- src/components/layout/BaseLayout.jsx
+++ src/components/layout/BaseLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { BaseLayoutWrap } from "./BaseLayout.Styles";
+import { BaseLayoutWrap } from "./BaseLayout.styles";
 import Sidebar from "../sidebar/Sidebar";
 import AppBar from "../appBar/AppBar";
 
